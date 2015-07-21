ec0b4aa836d5ad977453df9a20335da502a990f8
Kevin Kirsche <kev.kirsche@gmail.com>
2015-07-21 18:00:38 -0400

    Fix nessus / auth setup

--- lib/Nessus6.rb
+++ lib/Nessus6.rb
@@ -4,22 +4,37 @@ require 'Nessus6/editor/methods'
 require 'Nessus6/session/methods'
 
 module Nessus6
+  # The Client class is used to interact with the Nessus API
   class Client
     attr_reader :client, :editor, :session
-    def initialize(username, password, nessus_ip, nessus_port = '8834')
+
+    def initialize(credentials, nessus)
+      nessus[:port] = '8834' unless nessus.key?(:port)
+
       # Create our client
-      @client = Hurley::Client.new 'https://' + nessus_ip + ':' + nessus_port
+      @client = Hurley::Client.new 'https://' + nessus[:ip] + ':' + nessus[:port]
       @client.ssl_options.skip_verification = true
 
-      # Open up a session and get our token so we can make queries
-      @session = Nessus6::Session.new @client
-      @token = @session.create(username, password)
-      @client.header['X-Cookie'] = "token = #{@token}"
+      authenticate credentials
 
       @session = Nessus6::Session.new @client
       @editor = Nessus6::Editor.new @client
 
       @client
+    end
+
+    def authenticate(credentials)
+      # Open up a session and get our token so we can make queries
+      @session = Nessus6::Session.new @client
+      if credentials[:username] && credentials[:password]
+        @token = @session.create(credentials[:username], credentials[:password])
+        @client.header['X-Cookie'] = "token = #{@token}"
+      elsif credentials[:access_key] && credentials[:secret_key]
+      else
+        fail AuthenticationError, 'Authentication credentials not provided. ' \
+          'Must provided either username and password or access key and' \
+          ' secret key.'
+      end
     end
   end
 end
