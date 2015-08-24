27c52ca67233e326203f33efd2dbf6ddc6e06721
Kevin Kirsche <kev.kirsche@gmail.com>
2015-08-24 15:30:15 -0400

    Test the full Nessus client

--- test/spec/nessus6_spec.rb
+++ test/spec/nessus6_spec.rb
@@ -25,16 +25,8 @@ module Nessus6Test
       expect(@client).must_be_instance_of Nessus6::Client
     end
 
-    it 'should have a session client' do
-      expect(@client.session).must_be_instance_of Nessus6::Session
-    end
-
     it 'should have a editor client' do
       expect(@client.editor).must_be_instance_of Nessus6::Editor
-    end
-
-    it 'should have a user client' do
-      expect(@client.user).must_be_instance_of Nessus6::User
     end
 
     it 'should have a file client' do
@@ -53,12 +45,36 @@ module Nessus6Test
       expect(@client.permission).must_be_instance_of Nessus6::Permission
     end
 
+    it 'should have a plugin client' do
+      expect(@client.plugin).must_be_instance_of Nessus6::Plugin
+    end
+
+    it 'should have a plugin rules client' do
+      expect(@client.plugin_rule).must_be_instance_of Nessus6::PluginRule
+    end
+
     it 'should have a scan client' do
       expect(@client.scan).must_be_instance_of Nessus6::Scan
     end
 
+    it 'should have a scanner client' do
+      expect(@client.scanner).must_be_instance_of Nessus6::Scanner
+    end
+
+    it 'should have a server client' do
+      expect(@client.server).must_be_instance_of Nessus6::Server
+    end
+
+    it 'should have a session client' do
+      expect(@client.session).must_be_instance_of Nessus6::Session
+    end
+
     it 'should have a session token' do
       expect(@client.client.header['X-Cookie']).must_equal 'token = test_token'
+    end
+
+    it 'should have a user client' do
+      expect(@client.user).must_be_instance_of Nessus6::User
     end
   end
 end
