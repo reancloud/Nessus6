d610b5024f736c6f2256b1e3dedbfb171087582c
Kevin Kirsche <kev.kirsche@gmail.com>
2015-08-24 14:04:06 -0400

    Document the Error module / namespace

--- lib/Nessus6/errors/authentication_error.rb
+++ lib/Nessus6/errors/authentication_error.rb
@@ -1,4 +1,5 @@
 module Nessus6
+  # The Error module is used to house error conditions for the Nessus6 tool
   module Error
     # Authentication error is thrown when the user cannot authenticate with
     # Nessus
