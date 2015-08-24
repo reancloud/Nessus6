e0ffef42ccb0e944ce5b0bde435df8584265d333
Kevin Kirsche <kev.kirsche@gmail.com>
2015-08-24 11:15:33 -0400

    Include Plugin API code

---
+++ lib/Nessus6/plugin.rb
@@ -0,0 +1,50 @@
+module Nessus6
+  # The Plugin class is for interacting with Nessus6 plugins.
+  # https://localhost:8834/api#/resources/plugins
+  class Plugin
+    include Nessus6::Verification
+
+    public
+
+    def initialize(client)
+      @client = client
+    end
+
+    # Returns the list of plugin families. This request requires standard user
+    # permissions.
+    #
+    # @return [Hash]
+    def families
+      response = @client.get('plugins/families')
+      verify response,
+             forbidden: 'You do not have permission to view plugin families',
+             internal_server_error: 'Server failed to create the group'
+    end
+
+    # Returns the list of plugins in a family. This request requires standard
+    # user permissions.
+    #
+    # @param plugin_family_id [String] The id of the family to lookup.
+    # @return [Hash]
+    def family_details(plugin_family_id)
+      response = @client.get("plugins/families/#{plugin_family_id}")
+      verify response,
+             forbidden: 'You do not have permission to view the plugin family',
+             not_found: 'Plugin family not found',
+             internal_server_error: 'Server failed to create the group'
+    end
+
+    # Returns the details for a given plugin. This request requires standard
+    # user permissions.
+    #
+    # @param plugin_id [String] The id of the plugin.
+    # @return [Hash]
+    def plugin_details(plugin_id)
+      response = @client.get("plugins/plugin/#{plugin_id}")
+      verify response,
+             forbidden: 'You do not have permission to view the plugin',
+             not_found: 'Plugin not found',
+             internal_server_error: 'Server failed to create the group'
+    end
+  end
+end
